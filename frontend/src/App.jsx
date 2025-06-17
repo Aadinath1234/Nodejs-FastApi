4b9c17a9b9af51a69728e596306dcc2627e8550b
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2025-06-17 12:43:00 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,20 +10,71 @@ import Salary from "./components/Salary/Salary";
 import JobPosting from "./components/JobPosting/JobPosting";
 import Home from "./components/Home/Home";
 import Task from "./components/Task/Task";
+import ProtectedRedirection from "./components/ProtectedRedirection";
+import ProtectedRoute from "./components/Protectedroute";
 
 function App() {
   return (
     <div className=" overflow-x-hidden">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/salary" element={<Salary />} />
-          <Route path="/people" element={<People />} />
-          <Route path="/form" element={<JobPosting />} />
-          <Route path="/task" element={<Task />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRedirection>
+                <Home />
+              </ProtectedRedirection>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <ProtectedRedirection>
+                <Login />
+              </ProtectedRedirection>
+            }
+          />
+          <Route path="/signup" element={
+            <ProtectedRedirection>
+ <Signup />
+            </ProtectedRedirection>
+           
+            
+            } />
+          <Route path="/dashboard" element={
+            <ProtectedRedirection>
+<Dashboard />
+            </ProtectedRedirection>
+            
+            } />
+          <Route path="/salary" element={
+           <ProtectedRedirection>
+ <Salary />
+           </ProtectedRedirection>
+           
+            
+            } />
+          <Route path="/people" element={
+             <ProtectedRedirection>
+              <People />
+             </ProtectedRedirection>
+            
+            
+            } />
+          <Route path="/form" element={
+            <ProtectedRedirection>
+                <JobPosting />
+            </ProtectedRedirection>
+           
+            } />
+          <Route
+            path="/task"
+            element={
+              <ProtectedRoute>
+                <Task />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </div>
